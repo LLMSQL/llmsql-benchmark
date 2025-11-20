17b9dabd41867f89ef19f8cd01160144e91a2f32
Viknov <vika.nowogrodska@gmail.com>
2025-11-20 20:44:24 +0100

    warning + workflows

--- docs/conf.py
+++ docs/conf.py
@@ -9,21 +9,19 @@ release = "0.2.0"
 
 # -- General configuration ---------------------------------------------------
 
-extensions = [
-    "sphinx.ext.autodoc",
-    "sphinx.ext.napoleon",
-    "sphinx_autodoc_typehints",
-]
+extensions = []
 
-templates_path = []
-
+templates_path = ["_templates"]
 exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
 
-# Статика (CSS/JS)
 html_static_path = ["_static"]
 
-# ❌ Не используем тему, чтобы не ломала верстку
-html_theme = None
+html_theme = "basic"
 
-# ❗ Просто скопировать index.html как есть
-html_extra_path = ["_templates"]
+html_additional_pages = {
+    "index": "index.html",
+}
+
+def setup(app):
+    app.add_css_file("styles/front_page.css")
+    app.add_js_file("scripts/front_page.js")
